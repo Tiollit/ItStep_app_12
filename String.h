09bc7f0093fdfad0e5149a8e536ac7edfab7a613
Tiollit <Tiollit7@gmail.com>
2023-02-11 09:13:45 -0800

    f

--- String.h
+++ String.h
@@ -1,1 +1,25 @@
-#pragma once
+#include <iostream>
+using namespace std;
+
+class String {
+
+public:
+    string 
+
+    void set_PIB(string PIB);
+    string get_PIB();
+    void set_date(string date);
+    string get_date();
+    void set_phone(string phone);
+    string get_phone();
+    void set_country(string country);
+    string get_country();
+    void set_town(string town);
+    string get_town();
+    void set_study(string study);
+    string get_study();
+    void set_groupe(string groupe);
+    string get_groupe();
+
+};
+
