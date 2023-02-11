880022f4c449ff99dd5afd87feb1c7d2a5aabaf8
Tiollit <Tiollit7@gmail.com>
2023-02-11 05:42:42 -0800

    g

---
+++ Square.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Square {
+public:
+    string Triangle, Rectangle, Quadrate, Rhombus;
+
+    static void set_Triangle(string Triangle);
+    string get_Triangle();
+    static void set_Rectangle(string Rectangle);
+    string get_Rectangle();
+    static void set_Quadrate(string Quadrate);
+    string get_Quadrate();
+    static void set_Rhombus(string Rhombus);
+    string get_Rhombus();
+    
+};
