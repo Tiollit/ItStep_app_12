4aea9032ab10a7ce075efde72237fb1ef532781a
Tiollit <Tiollit7@gmail.com>
2023-02-11 06:03:13 -0800

    h

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string> 
+#include "Square.h"
+using namespace std;
+
+void main()
+{
+	int k;
+	cout << "Choose the figure for square calculation:" << endl;
+	cout << " 1 for Triangle," << endl;
+	cout << " 2 for Rectangle," << endl;
+	cout << " 3 for Quadrante," << endl;
+	cout << " 4 for Rhombus," << endl;
+	cin >> k;
+	switch(k)
+}
