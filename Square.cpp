73a43bfca3f7240b63c5250c238060d03802bab5
Tiollit <Tiollit7@gmail.com>
2023-02-11 06:51:51 -0800

    g

--- Square.cpp
+++ Square.cpp
@@ -1,6 +1,6 @@
 #include "Square.h"
 
-void Square::set_S1(int S1)
+int Square::set_S1(int S1)
 {
   S1 = S1;
 }
@@ -9,7 +9,7 @@ int Square::get_S1()
     return S1;
 }
 
-void Square::set_S2(int S2)
+int Square::set_S2(int S2)
 {
    S2 = S2;
 }
@@ -18,7 +18,7 @@ int Square::get_S2()
         return S2;   
 }
 
-void Square::set_S3(int S3)
+int Square::set_S3(int S3)
 {
    S3 = S3;
 }
--- Square.h
+++ Square.h
@@ -9,6 +9,6 @@ public:
     int get_S1();
     static int set_S2(int S2);
     int get_S2();
-    static int set_S2(int S3);
+    static int set_S3(int S3);
     int get_S3();    
 };
