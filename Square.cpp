cfd1a64cfd1db985fd0e16c6fcec08016e7ec875
Tiollit <Tiollit7@gmail.com>
2023-02-11 05:58:53 -0800

    b

---
+++ Square.cpp
@@ -0,0 +1,37 @@
+#include "Square.h"
+
+void Square::set_Triangle(int Triangle)
+{
+    this->Triangle = Triangle;
+}
+int Square::get_Triangle()
+{
+    return Triangle;
+}
+
+void Square::set_Rectangle(int Rectangle)
+{
+    this->Rectangle = Rectangle;
+}
+int Square::get_Rectangle()
+{   
+        return Rectangle;   
+}
+
+void Square::set_Quadrate(int Quadrate)
+{
+    this->Quadrate = Quadrate;
+}
+int Square::get_Quadrate()
+{    
+        return Quadrate;    
+}
+
+void Square::set_Rhombus(int Rhombus)
+{
+    this->Rhombus = Rhombus;
+}
+int Square::get_Rhombus()
+{
+    return Rhombus;
+}
--- Square.h
+++ Square.h
@@ -3,15 +3,15 @@ using namespace std;
 
 class Square {
 public:
-    string Triangle, Rectangle, Quadrate, Rhombus;
+    int Triangle, Rectangle, Quadrate, Rhombus;
 
-    static void set_Triangle(string Triangle);
-    string get_Triangle();
-    static void set_Rectangle(string Rectangle);
-    string get_Rectangle();
-    static void set_Quadrate(string Quadrate);
-    string get_Quadrate();
-    static void set_Rhombus(string Rhombus);
-    string get_Rhombus();
+    static void set_Triangle(int Triangle);
+    int get_Triangle();
+    static void set_Rectangle(int Rectangle);
+    int get_Rectangle();
+    static void set_Quadrate(int Quadrate);
+    int get_Quadrate();
+    static void set_Rhombus(int Rhombus);
+    int get_Rhombus();
     
 };
